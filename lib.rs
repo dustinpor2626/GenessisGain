9d3294b2b5621c70aff44f244be078dcb6dc9ff1
Sourav Singh <66918899+dustinpor2626@users.noreply.github.com>
2023-12-02 18:04:47 +0530

    Update lib.rs

--- lib.rs
+++ lib.rs
@@ -1,81 +1,96 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
-use ink::storage::Mapping;
 
 
 #[ink::contract]
 mod genesisgain {
+    use ink::{storage::Mapping, env::account_id};
+
+        #[ink(event)]
+        pub struct LogMessage {
+            #[ink(topic)]
+            message: AccountId,
+        }
 
     type doner = ink_prelude::vec::Vec<(AccountId,Balance)>;
-    type accountId = AccountId;
-    type post = u64;
+    type post = ink_prelude::vec::Vec<u8>;
 
     #[ink(storage)]
+    #[derive(Default)]
     pub struct Genesisgain{
-        posts: Mapping<accountId, Vec<post>>,
-        doners:Mapping<accountId, doner>,
-        accountIds: Mapping<AccountId, ()>,
-    }
-
-    pub struct profile{
-        owner : AccountId,
-        posts : ink_prelude::vec::Vec<profile>,
+        posts: Mapping<AccountId, post>,
+        doners: Mapping<AccountId, doner>,
     }
     
     impl Genesisgain {
         #[ink(constructor)]
-        pub fn new(content: ink_prelude::vec::Vec<u8>, owner: AccountId) -> Self {
+        pub fn new(content: ink_prelude::vec::Vec<u8>, owner: AccountId,doner:ink_prelude::vec::Vec<(AccountId,Balance)>) -> Self {
+        /*        let mut my_map: Mapping<ink::primitives::AccountId,ink_prelude::vec::Vec<u8>, ink::storage::traits::ResolverKey<ink::storage::traits::AutoKey, ink::storage::traits::ManualKey<1994045837>>> = Mapping::default();
+                let caller = Self::env().caller();
+                my_map.insert(&caller,&content);
+               let mut my_map2: Mapping<ink::primitives::AccountId, ink_prelude::vec::Vec<(AccountId,Balance)>, ink::storage::traits::ResolverKey<ink::storage::traits::AutoKey, ink::storage::traits::ManualKey<3997244855>>> = Mapping::default();
+                my_map2.insert(&caller, &doner);
             Self {
-                posts:0,
-                doners:0,
-                accountIds: 0,
-            }
+                posts:my_map,
+                doners:my_map2,
+            }*/
+            Default::default()
         }
 
-        #[ink(message)]
+       /*#[ink(message)]
         pub fn post_content(&mut self, new_content: ink_prelude::vec::Vec<u8>) {
             let account_id = self.env().caller();
-            let &mut posts = self.posts.get(account_id);
-            if posts.length() == 0{
-                posts = [];
-            }
-            posts.append(new_content);
-            self.posts.insert(account_id, posts);
+            let mut posts = match self.posts.get(&account_id) {
+                Some(existing_posts) => existing_posts.clone(),
+                None => ink_prelude::vec::Vec::new(),
+            };
+            posts.extend(new_content);
+            self.posts.insert(account_id, &posts);
+        }*/
 
-            if(!self.accountIds.get(account_id).is_some()){
-                 self.accountIds.insert(account_id, ());
-            }
-        }
 
         #[ink(message)]
         pub fn donate(&mut self, amount: Balance, post_owner: AccountId) {
-            let &mut donerList = self.doners.get(post_owner);
+            let mut donerList = match self.doners.get(&post_owner) {
+                Some(existing_donors) => existing_donors.clone(),
+                None => ink_prelude::vec::Vec::new(),
+            };
+            //need to write transaction logic            
             donerList.push((self.env().caller(),amount));
-            self.doners.insert((self.env().caller(),donerList));
+            self.doners.insert(post_owner,&donerList);
+            self.env().emit_event(LogMessage {
+                    message: AccountId::from(post_owner),
+                });
         }
 
         
-        #[ink(message)]
-        pub fn get_random_post(&self) -> Option<(
-            ink_prelude::vec::Vec<ink_prelude::vec::Vec<(AccountId,Balance)>>,
-        )> {
-            let len = self.accountIds.len();
-            if len == 0 {
-                return None;
-            }
-            let _random_index = 1;
-            let &mut account = self.accountIds.get(&_random_index);
-            let post1 = self.posts.get(account).clone();
-            let account = self.accountIds.get(&2);
-            let post2 = self.posts.get(account).clone();
-            let posts=  vec![];
-            posts.push(post1);
-            posts.push(post2);
-            Some(posts)
+         /*   #[ink(message)]
+            pub fn get_random_post(&mut self,post_owner: AccountId) -> Option<(
+                ink_prelude::vec::Vec<u8>
+            )> {
+                let post_list = self.posts.get(post_owner).expect("No Post till now");
+                Some((post_list))
         }
+*/
+
+        #[ink(message)]
+        pub fn get_doner_list(&mut self,post_owner: AccountId) -> Option<(
+            ink_prelude::vec::Vec<(AccountId,Balance)>
+        )>{
+            let result = self.doners.get(&post_owner).map(|donerList| donerList.clone());
+            if result.is_some() {
+                return result
+            } else {
+                self.env().emit_event(LogMessage {
+                    message: AccountId::from(post_owner),
+                });
+                return None
+            }
+        }
+
+        
 
     }
 }
-
 
 
 
